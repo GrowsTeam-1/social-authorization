164bb4f109092fcc911b2c948bf524798fa8b9d4
FedenkoM <fedenkomisha@gmail.com>
2021-08-05 16:20:04 +0300

    initial commit

---
+++ src/main/java/com/fedenkom/socialauthorization/SocialAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.fedenkom.socialauthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialAuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialAuthorizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fedenkom/socialauthorization/SocialAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fedenkom.socialauthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialAuthorizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
